274edfd870f8207cdfe4adbe82686003f0cedae6
Usama <learnbeginners@gmail.com>
2018-11-11 20:44:45 +0500

    Retrieving list of liked user and likedby user in API

--- DatingApp.API/Data/DatingRepository.cs
+++ DatingApp.API/Data/DatingRepository.cs
@@ -40,6 +40,17 @@ namespace DatingApp.API.Data
 
             users = users.Where(u => u.Id != userParams.UserId);
             users = users.Where(u => u.Gender == userParams.Gender);
+
+            if (userParams.Likers)
+            {
+                var userLikers = await GetUserLikes(userParams.UserId, userParams.Likers);
+                users = users.Where(u => userLikers.Contains(u.Id));
+            }
+            if (userParams.Likees)
+            {
+                var userLikees = await GetUserLikes(userParams.UserId, userParams.Likers);
+                users = users.Where(u => userLikees.Contains(u.Id));
+            }
             
             if (userParams.MinAge != 18 || userParams.MaxAge != 99)
             {
@@ -66,6 +77,23 @@ namespace DatingApp.API.Data
             return await PagedList<User>
                 .CreateAsync(users, userParams.PageNumber, userParams.PageSize);
         }        
+
+        private async Task<IEnumerable<int>> GetUserLikes(int id, bool likers)
+        {
+            var user = await _context.Users
+                .Include(u => u.Likers)
+                .Include(u => u.Likees)
+                .FirstOrDefaultAsync(u => u.Id == id);
+
+            if (likers)
+            {
+                return user.Likers.Where(u => u.LikeeId == id).Select(i => i.LikerId);
+            }
+            else 
+            {
+                return user.Likees.Where(u => u.LikerId == id).Select(i => i.LikeeId);
+            }
+        }
 
         public async Task<Photo> GetPhoto(int id)
         {
