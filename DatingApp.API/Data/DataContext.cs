4a78ab3bd724d925a03c7e16bda5558674e24bfe
Usama <learnbeginners@gmail.com>
2018-11-12 01:33:02 +0500

    Many to Many relationship of User and Messages

--- DatingApp.API/Data/DataContext.cs
+++ DatingApp.API/Data/DataContext.cs
@@ -14,11 +14,13 @@ namespace DatingApp.API.Data
         public DbSet<User> Users { get; set; }
         public DbSet<Photo> Photos { get; set; }
         public DbSet<Like> Likes { get; set; }
+        public DbSet<Message> Messages { get; set; }
         protected override void OnModelCreating(ModelBuilder builder)
         {
             builder.Entity<Like>()
                 .HasKey(k => new { k.LikeeId, k.LikerId });
 
+            // it is a junction table containing information of likers & likees
             builder.Entity<Like>()
                 .HasOne<User>(u => u.Likee)
                 .WithMany(u => u.Likers)
@@ -29,6 +31,18 @@ namespace DatingApp.API.Data
                 .HasOne<User>(u => u.Liker)
                 .WithMany(u => u.Likees)
                 .HasForeignKey(u => u.LikerId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            // it is also a junction table but have extra fields.
+            // So it is an Entity itself.
+            builder.Entity<Message>()
+                .HasOne<User>(u => u.Sender)
+                .WithMany(u => u.MessageSent)
+                .OnDelete(DeleteBehavior.Restrict);
+                
+            builder.Entity<Message>()
+                .HasOne<User>(u => u.Recipient)
+                .WithMany(u => u.MessageReceived)
                 .OnDelete(DeleteBehavior.Restrict);
         }
     }
