e124779db43ae45edd717a72c05117f9ee1590e9
Usama <learnbeginners@gmail.com>
2018-11-11 18:30:38 +0500

    Liker, Likee Relationship and Like Functionality in API

--- DatingApp.API/Data/DatingRepository.cs
+++ DatingApp.API/Data/DatingRepository.cs
@@ -82,5 +82,11 @@ namespace DatingApp.API.Data
         {
             return await _context.SaveChangesAsync() > 0;
         }
+
+        public async Task<Like> GetLike(int userId, int recipientId)
+        {
+            return await _context.Likes.FirstOrDefaultAsync(u => 
+                u.LikerId == userId && u.LikeeId == recipientId);
+        }
     }
 }
