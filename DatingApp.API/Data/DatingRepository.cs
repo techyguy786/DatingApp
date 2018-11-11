1f40a7193eb7fe964a03711ad266283736671c0a
Usama <learnbeginners@gmail.com>
2018-11-12 02:40:23 +0500

    Sending Messages and Adding into DB (API)

--- DatingApp.API/Data/DatingRepository.cs
+++ DatingApp.API/Data/DatingRepository.cs
@@ -116,5 +116,20 @@ namespace DatingApp.API.Data
             return await _context.Likes.FirstOrDefaultAsync(u => 
                 u.LikerId == userId && u.LikeeId == recipientId);
         }
+
+        public async Task<Message> GetMessage(int id)
+        {
+            return await _context.Messages.FirstOrDefaultAsync(m => m.Id == id);
+        }
+
+        public Task<PagedList<Message>> GetMessagesForUser()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Message>> GetMessageThread(int userId, int recipientId)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- DatingApp.API/Helpers/AutoMapperProfile.cs
+++ DatingApp.API/Helpers/AutoMapperProfile.cs
@@ -31,6 +31,8 @@ namespace DatingApp.API.Helpers
             CreateMap<Photo, PhotoForReturnDto>();
             CreateMap<PhotoForCreationDto, Photo>();
             CreateMap<UserForRegisterDto, User>();
+            // Now it will work in both directions
+            CreateMap<MessageForCreationDto, Message>().ReverseMap();
         }
     }
 }
