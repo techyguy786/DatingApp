5fef4ed6338b48a4b593b9f7c937633df5e011c6
Usama <learnbeginners@gmail.com>
2018-11-12 23:45:41 +0500

    Sending Messages in SPA

--- DatingApp.API/Controllers/MessagesController.cs
+++ DatingApp.API/Controllers/MessagesController.cs
@@ -80,7 +80,9 @@ namespace DatingApp.API.Controllers
         public async Task<IActionResult> CreateMessage(int userId,
              MessageForCreationDto messageForCreationDto)
         {
-            if (userId != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
+            var sender = _repo.GetUser(userId);
+
+            if (sender.Id != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
                 return Unauthorized();
 
             messageForCreationDto.SenderId = userId;
@@ -94,10 +96,10 @@ namespace DatingApp.API.Controllers
 
             _repo.Add(message);
 
-            var messageToReturn = _mapper.Map<MessageForCreationDto>(message);
-
-            if (await _repo.SaveAll())
+            if (await _repo.SaveAll()) {
+                var messageToReturn = _mapper.Map<MessageToReturnDto>(message);
                 return CreatedAtRoute("GetMessage", new { id = message.Id }, messageToReturn);
+            }
 
             throw new Exception("Creating Messages Failed on Save");
         }
