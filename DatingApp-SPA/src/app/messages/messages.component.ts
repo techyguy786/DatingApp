0d858d30ab3632cdf509a130faf59524689d92cf
Usama <learnbeginners@gmail.com>
2018-11-13 01:00:03 +0500

    Deleting Messages in SPA

--- DatingApp-SPA/src/app/messages/messages.component.ts
+++ DatingApp-SPA/src/app/messages/messages.component.ts
@@ -41,6 +41,18 @@ export class MessagesComponent implements OnInit {
       });
   }
 
+  deleteMessage(id: number) {
+    this.alertify.confirm('Are you sure you want to delete the message', () => {
+      this.userService.deleteMessage(id, this.authService.decodedToken.nameid)
+      .subscribe(() => {
+        this.messages.splice(this.messages.findIndex(m => m.id === id), 1);
+        this.alertify.success('Message has been deleted');
+      }, error => {
+        this.alertify.error('Failed to Delete the Message');
+      });
+    });
+  }
+
   pageChanged(event: any): void {
     this.pagination.currentPage = event.page;
     this.loadMessages();
